f032e8c823e53eeaee42d5dfb0eaa18a18c7e577
JitendraBordiya <Jitendra7496@gmail.com>
2021-09-27 09:45:49 +0530

    Project module and Api s

---
+++ HRMSApi/FunctionData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HRMSApi
+{
+
+    public class FunctionData
+    {
+
+        public string Employee_ID
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string EmailId
+        {
+            get;
+            set;
+        }
+
+
+        public string ContactNo
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string Salary
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
